bd1d46bf727f07c982ece49dd77d29b3d42fd223
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-12-08 14:12:33 -0800

    added get to doubly LL

--- dataStructures/doubllyLinkedList.js
+++ dataStructures/doubllyLinkedList.js
@@ -53,5 +53,24 @@ class Node {
       this.length--;
       return oldHead;
     }
+    get(index) {
+      if(index < 0 || index<= this.length) return null;
+      var count, current;
+      if(index <= this.length / 2) {
+        count = 0;
+        current = this.head;
+        while(count !== index) {
+          current = current.next;
+          count++;
+        }
+      } else {
+        count = this.length -1;
+        current = this.tail;
+        while(count !== index) {
+          current = current.prev;
+          count--;
+        }
+      }
+      return current;
+    }
   }
-}
