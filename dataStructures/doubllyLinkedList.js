92c5661a9f518c5416f59882f5d9a72ee0cf8edf
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-12-08 14:14:45 -0800

    updated doubly LL

--- dataStructures/doubllyLinkedList.js
+++ dataStructures/doubllyLinkedList.js
@@ -6,71 +6,74 @@ class Node {
   }
 }
 
-  class DoublyLinkedList {
-    constructor() {
-      this.head = null;
-      this.tail = null'
-      this.length = 0;
-    }
-    push(val) {
-      var newNode = new Node(val);
-      if(this.length === 0) {
-        this.head = newNode;
-        this.tail = newNode;
-      } else {
-        this.tail.next = newNode;
-        newNode.prev = this.tail;
-        this.tail = newNode;
-      }
-      this.length++;
-      return this;
-    }
-    pop() {
-      if(!this.head) return undefined;
-      var poppedNode = this.tail;
-      if(this.length === 1) {
-        this.head = null;
-        this.tail = null;
-      } else {
-        this.tail = poppedNode.prev;
-        this.tail.next = null;
-        poppedNode.prev = null;
-      }
-      this.length--;
-      return poppedNode;
-    }
-    shift() {
-      if(this.length ===  0) return undefined;
-      var oldHead = this.head;
-      if(this.length === 1) {
-        this.head = null;
-        this.tail = null;
-      } else {
-        this.head = oldHead.next;
-        this.head.prev = null;
-        oldHead.next = null;
-      }
-      this.length--;
-      return oldHead;
-    }
-    get(index) {
-      if(index < 0 || index<= this.length) return null;
-      var count, current;
-      if(index <= this.length / 2) {
-        count = 0;
-        current = this.head;
-        while(count !== index) {
-          current = current.next;
-          count++;
-        }
-      } else {
-        count = this.length -1;
-        current = this.tail;
-        while(count !== index) {
-          current = current.prev;
-          count--;
-        }
-      }
-      return current;
-    }
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
+  push(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    var poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+  shift() {
+    if (this.length === 0) return undefined;
+    var oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+  get(index) {
+    if (index < 0 || index <= this.length) return null;
+    var count, current;
+    if (index <= this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+    }
+    return current;
+  }
+}
+
+var list = new DoublyLinkedList();
+list.push("yo");
